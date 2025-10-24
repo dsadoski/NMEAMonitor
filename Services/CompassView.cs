9f5aeec5f7b45acaa64bbce9767e61d4ff9cf3a3
dsadoski <115582410+dsadoski@users.noreply.github.com>
2025-10-24 19:37:03 -0400

    This will not work no graphics primitive support

---
+++ Services/CompassView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMEAMon.Services
+{
+    using Microsoft.Maui.Controls;
+    using Microsoft.Maui.Graphics;
+
+    public class GraphicsPage : ContentPage
+    {
+        readonly MyDrawable drawable = new();
+
+        public GraphicsPage()
+        {
+            Title = "Native Graphics Page";
+
+            var graphicsView = new GraphicsView
+            {
+                Drawable = drawable,
+                HeightRequest = 300,
+                WidthRequest = 300
+            };
+
+            Content = new VerticalStackLayout
+            {
+                Padding = 20,
+                Spacing = 10,
+                Children =
+            {
+                new Label
+                {
+                    Text = "This is a native MAUI GraphicsView",
+                    FontSize = 20,
+                    HorizontalOptions = LayoutOptions.Center
+                },
+                graphicsView
+            }
+            };
+        }
+    }
+
+    public class MyDrawable : IDrawable
+    {
+        float radius = 40;
+        bool growing = true;
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            canvas.FillColor = Colors.CornflowerBlue;
+            canvas.FillRectangle(dirtyRect);
+
+            canvas.FillColor = Colors.White;
+            canvas.FillCircle(dirtyRect.Center.X, dirtyRect.Center.Y, radius);
+
+            // Animate
+            radius += growing ? 1 : -1;
+            if (radius >= 100 || radius <= 20)
+                growing = !growing;
+        }
+    }
+}
